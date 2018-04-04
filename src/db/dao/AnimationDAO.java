d6d2c0f088414a9c1b795e947c94f2a1b1f5d0c6
Krowk <lion.adam.otman@gmail.com>
2018-04-04 18:37:55 +0200

    ddd

--- src/db/dao/AnimationDAO.java
+++ src/db/dao/AnimationDAO.java
@@ -11,10 +11,10 @@ public class AnimationDAO extends DAO {
 	@Override
 	public Object f(String codGeo) {
 		try {
-			String query = "SELECT ETAZ10 FROM agriculture WHERE codGeo="+ codGeo+ " limit 1";
+			String query = "SELECT jourDebut, moisDebut, anneeDebut, themes, libelleOrg from evenement where codGeo=41018 ORDER BY anneeDebut DESC, moisDebut DESC, jourDebut DESC limit 5";
 			ResultSet rs = Application.passQuery(query);
 			rs.next();
-			Agriculture agriculture = new Agriculture();
+			Animation animation = new Animation();
 			agriculture.setNombre_entreprise_agriculture(rs.getInt(1));
 			return agriculture;
 		} catch (SQLException e) {
